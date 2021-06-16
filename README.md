1c08db7fa7e16793d17d4c7d6c7cf5e4d01d6a32
CesarNBR <buendia732@gmail.com>
2021-06-16 13:02:07 -0500

    proyecto

---
+++ README.md
@@ -0,0 +1,4 @@
+## Environment variables
+* PORT, server port 8080
+* NOTES_APP_MONGODB_HOST=localhost
+* NOTES_APP_MONGODB_DATABASE=notes-app
---
+++ src/routes/note.routes.js
@@ -0,0 +1,30 @@
+const { Router } = require('express')
+const router = Router()
+
+const { 
+    renderNoteForm, 
+    createNewNote, 
+    renderNotes, 
+    renderEditForm, 
+    updateNote, 
+    deletenote } = require('../controllers/notes.controller');
+
+const {isAuthenticated} = require('../helpers/auth')
+
+//New Note
+router.get('/notes/add',isAuthenticated, renderNoteForm);
+
+router.post('/notes/new-note',isAuthenticated, createNewNote);
+
+//Get all note
+router.get('/notes',isAuthenticated, renderNotes)
+
+//  Edit notes
+router.get('/notes/edit/:id',isAuthenticated, renderEditForm)
+
+router.put('/notes/edit/:id',isAuthenticated, updateNote)
+
+//Delete Notes
+router.delete('/notes/delete/:id',isAuthenticated, deletenote)
+
+module.exports = router
